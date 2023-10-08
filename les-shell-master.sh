2ec4cc0651492718f1db5c22fe50369f7ce1835c
BartTho <61376948+BartTho@users.noreply.github.com>
2023-10-08 16:42:43 +0200

    script les-shell-master.sh

--- les-shell-master.sh
+++ les-shell-master.sh
@@ -1,2 +1,15 @@
-wget -O - https://github.com/BartTho/Les/raw/master/demo0.tar
-tar -xvf demo.tar .
+wget -O - https://github.com/BartTho/Les/raw/master/a
+wget -O - https://github.com/BartTho/Les/raw/master/a1.txt
+wget -O - https://github.com/BartTho/Les/raw/master/a10
+wget -O - https://github.com/BartTho/Les/raw/master/a2
+wget -O - https://github.com/BartTho/Les/raw/master/a20
+wget -O - https://github.com/BartTho/Les/raw/master/aA0
+wget -O - https://github.com/BartTho/Les/raw/master/aB0
+wget -O - https://github.com/BartTho/Les/raw/master/abc
+wget -O - https://github.com/BartTho/Les/raw/master/abcd
+wget -O - https://github.com/BartTho/Les/raw/master/cB0
+wget -O - https://github.com/BartTho/Les/raw/master/geel
+wget -O - https://github.com/BartTho/Les/raw/master/groen
+wget -O - https://github.com/BartTho/Les/raw/master/rood
+wget -O - https://github.com/BartTho/Les/raw/master/services
+rm les-shell-master.sh
