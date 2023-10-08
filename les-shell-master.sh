1a567bdd18c7b25f249fcb09af295b0202b75958
BartTho <61376948+BartTho@users.noreply.github.com>
2023-10-08 16:55:43 +0200

    Update les-shell-master.sh

--- les-shell-master.sh
+++ les-shell-master.sh
@@ -1,15 +1,2 @@
-wget -O - https://github.com/BartTho/Les/raw/master/a
-wget -O - https://github.com/BartTho/Les/raw/master/a1.txt
-wget -O - https://github.com/BartTho/Les/raw/master/a10.txt
-wget -O - https://github.com/BartTho/Les/raw/master/a2.txt
-wget -O - https://github.com/BartTho/Les/raw/master/a20.txt
-wget -O - https://github.com/BartTho/Les/raw/master/aA0.txt
-wget -O - https://github.com/BartTho/Les/raw/master/aB0.txt
-wget -O - https://github.com/BartTho/Les/raw/master/abc.txt
-wget -O - https://github.com/BartTho/Les/raw/master/abcd.txt
-wget -O - https://github.com/BartTho/Les/raw/master/cB0.txt
-wget -O - https://github.com/BartTho/Les/raw/master/geel
-wget -O - https://github.com/BartTho/Les/raw/master/groen
-wget -O - https://github.com/BartTho/Les/raw/master/rood
-wget -O - https://github.com/BartTho/Les/raw/master/services
+wget -O - https://github.com/BartTho/Les/raw/master/demo.zip
 rm les-shell-master.sh
