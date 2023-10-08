d2d5c70a73be7dd3349691d48d81ac2b99e64022
BartTho <61376948+BartTho@users.noreply.github.com>
2023-10-08 16:59:29 +0200

    Update les-shell-master.sh

--- les-shell-master.sh
+++ les-shell-master.sh
@@ -1,4 +1,4 @@
-wget -O - https://github.com/BartTho/Les/raw/master/demo.zip
+wget -O - https://github.com/BartTho/Les/blob/ee68f6734a545e686f9f6d1b472e4e79ea6b1d70/demo.zip
 unzip demo.zip
 rm les-shell-master.sh
 rm demo.zip
